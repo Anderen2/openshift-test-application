5de6187fd51792932feced9ff950a5fdc138aa1f
SebastianJarsve <sebastianjarsve@ymail.com>
2016-04-12 18:55:30 +0200

    Added forms and a for loop test

--- structchat/urls.py
+++ structchat/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'web.views.index'),
+	url(r'^$', 'web.views.index'),
 
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(admin.site.urls)),
 )
--- web/views.py
+++ web/views.py
@@ -4,16 +4,19 @@ from django.template import RequestContext, loader
 
 # Create your views here.
 def index (request):
-    template = loader.get_template("index.html")
-    context = RequestContext(request, {
-        "author":"Sebastian",
-        "date":"1 hour ago",
-        "likes":23,
-        "content":"""Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
-    })
-    return HttpResponse(template.render(context))
+	template = loader.get_template("index.html")
+	context = RequestContext(request, {
+		"author":"Sebastian",
+		"date":"1 hour ago",
+		"likes":23,
+		"loop":range(3),
+		"content":(
+			"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod" +
+			"tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam," +
+			"quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo" + 
+			"consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse" +
+			"cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non" +
+			"proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+		)
+	})
+	return HttpResponse(template.render(context))
