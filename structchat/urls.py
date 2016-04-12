5f863c49187ccc128bf323607a5fd2254c8e8d31
SebastianJarsve <sebastianjarsve@ymail.com>
2016-04-12 12:56:10 +0200

    Design prototype

--- structchat/urls.py
+++ structchat/urls.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'web.views.index'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
