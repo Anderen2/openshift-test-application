e4406e96079469f502e5578f5154c55fefa8d2a1
SebastianJarsve <sebastianjarsve@ymail.com>
2016-04-20 20:15:18 +0200

    I'm so commited to struct-chat.

--- web/models.py
+++ web/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MessageModel (models.Model):
+    user_id = models.ForeignKey(
+        'UserModel',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+    room_id = models.ForeignKey(
+        'RoomModel',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+    timestamp = models.DateTimeField()
+    content = models.TextField()
+    rating = models.TextField() # If I gave you a nickle, would you tickle my pickle?
+
+
+class UserModel (models.Model):
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=256)
+
+class RoomModel (models.Model):
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=1024)
