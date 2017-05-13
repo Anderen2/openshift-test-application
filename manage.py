7a38fe4830f37a95e373c1e8eec5e5c89e9d34af
Andreas Skoglund <anderen95@gmail.com>
2017-05-13 20:55:21 +0200

    NR

--- manage.py
+++ manage.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
+import newrelic.agent
+newrelic.agent.initialize()
+
+print("Newrelic init")
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "structchat.settings")
