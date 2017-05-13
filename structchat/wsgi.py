e3cdd0d500c347806da7750a2cdda26316c0dd97
Andreas Skoglund <anderen95@gmail.com>
2017-05-13 21:12:40 +0200

    Gogo

--- structchat/wsgi.py
+++ structchat/wsgi.py
@@ -11,4 +11,4 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "structchat.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = newrelic.agent.WSGIApplicationWrapper(get_wsgi_application())
+application = get_wsgi_application()
